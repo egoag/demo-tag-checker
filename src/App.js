40d5fdd8b3f5e817f8d3823dc46f57ea59683c44
hank <hank@hanks-MacBook-Pro.local>
2019-09-06 14:44:16 +1200

    added eslint and format code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import './App.css'
+import Checker from './lib/checker'
+
+const App = () => {
+  const [text, setText] = useState()
+  const [result, setResult] = useState()
+  const check = () => {
+    let newResult
+    try {
+      newResult = Checker(text)
+    } catch (err) {
+      newResult = err.message
+      console.log(err.stack)
+    }
+    setResult(newResult)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tag Checker</h1>
+      <h2>Input text below and click Check</h2>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          check()
+        }}
+      >
+        <textarea
+          cols={100}
+          rows={20}
+          onChange={event => setText(event.target.value)}
+          placeholder="Text"
+        />
+        <div>
+          <button>Check</button>
+        </div>
+      </form>
+      <p>{result}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
